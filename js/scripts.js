cc43d7ad01c3262b861e30b7fcf5f3242fbf6875
Evelyn Fenner-Dorrity <evelyn@>
2017-08-15 11:56:03 -0700

    Change forEach to For loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,16 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    blanks.forEach(function(blank){
-      var userInput = $("input#" + blank).val();
-      $("." + blank).text(userInput);
-    });
-    
+    // blanks.forEach(function(blank){
+    //   var userInput = $("input#" + blank).val();
+    //   $("." + blank).text(userInput);
+    // });
+
+    for (var i = 0; i <= blanks.length; i += 1) {
+      var userInput = $("input#" + blanks[i]).val();
+      $("." + blanks[i]).text(userInput);
+    }
+
     $("#story").show();
 
     event.preventDefault();
